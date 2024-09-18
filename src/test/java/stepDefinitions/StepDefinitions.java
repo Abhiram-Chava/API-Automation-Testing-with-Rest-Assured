2485632dd3f890f158fe3d96069c6024eae4491c
Abhiram-Chava <abhirammetaqa@gmail.com>
2024-09-18 21:05:25 +0530

    Hex

--- src/test/java/stepDefinitions/StepDefinitions.java
+++ src/test/java/stepDefinitions/StepDefinitions.java
@@ -3,64 +3,48 @@ package stepDefinitions;
 import static org.junit.Assert.*;
 import static io.restassured.RestAssured.given;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import java.io.IOException;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import io.restassured.RestAssured;
-import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
-import pojo.AddPlace;
-import pojo.Location;
+import resources.APIResources;
+import resources.TestDataBuild;
+import resources.Utils;
 
-public class StepDefinitions {
+public class StepDefinitions extends Utils{
 
 	RequestSpecification res;
 	ResponseSpecification resspec;
 	Response response;
 	
-	@Given("Add Place Payload")
-	public void add_place_payload() {
+	TestDataBuild data = new TestDataBuild();
+	
+	@Given("Add Place Payload with {string} {string} {string}")
+	public void add_place_payload_with(String name, String language, String address) throws IOException {
 	    // Write code here that turns the phrase above into concrete actions
-		RestAssured.baseURI="https://rahulshettyacademy.com";
-
-		AddPlace p =new AddPlace();
-		p.setAccuracy(50);
-		p.setAddress("29, side layout, cohen 09");
-		p.setLanguage("French-IN");
-		p.setPhone_number("(+91) 983 893 3937");
-		p.setWebsite("https://rahulshettyacademy.com");
-		p.setName("Frontline house");
-		List<String> myList =new ArrayList<String>();
-		myList.add("shoe park");
-		myList.add("shop");
-
-		p.setTypes(myList);
-		Location l =new Location();
-		l.setLat(-38.383494);
-		l.setLng(33.427362);
-		p.setLocation(l);
-
-		 RequestSpecification req =new RequestSpecBuilder().setBaseUri("https://rahulshettyacademy.com").addQueryParam("key", "qaclick123")
-		.setContentType(ContentType.JSON).build();
 		 
-		 
-		resspec =new ResponseSpecBuilder().expectStatusCode(200).expectContentType(ContentType.JSON).build();
-		res=given().spec(req)
-		.body(p);
+		res=given().spec(requestSpecification())
+		.body(data.addPlacePayLoad(name, language, address));
 	}
-	@When("user calls {string} with Post http request")
-	public void user_calls_with_post_http_request(String string) {
+	@When("user calls {string} with {string} http request")
+	public void user_calls_with_http_request(String resource, String method) {
 	    // Write code here that turns the phrase above into concrete actions
-		response =res.when().post("/maps/api/place/add/json").
-				then().spec(resspec).extract().response();
+		APIResources resourceAPI = APIResources.valueOf(resource);
+		System.out.println(resourceAPI.getResource());
+		
+		resspec = new ResponseSpecBuilder().expectStatusCode(200).expectContentType(ContentType.JSON).build();
+		
+		if(method.equalsIgnoreCase("POST"))
+			response =res.when().post(resourceAPI.getResource());
+		else
+			response =res.when().get(resourceAPI.getResource());
+		
 	}
 	@Then("the API call got success with status code {int}")
 	public void the_api_call_got_success_with_status_code(Integer int1) {
--- target/test-classes/features/placeValidations.feature
+++ target/test-classes/features/placeValidations.feature
@@ -1,8 +1,13 @@
 Feature: Validating Place API's
 
 Scenario: Verify if Place is being Successfully added using AddPlaceAPI
-		Given Add Place Payload
-		When user calls "AddPlaceAPI" with Post http request
+		Given Add Place Payload with "<name>" "<language>" "<address>"
+		When user calls "AddPlaceAPI" with "POST" http request
 		Then the API call got success with status code 200
 		And "status" in response body is "OK"
 		And "scope" in response body is "APP"
+		
+Examples:
+		|	name		|	language	|	address						|
+		|	AAhouse	|	English		|	World cross center|
+		|	BBhouse	|	Spanish		|	Sea cross center	|
